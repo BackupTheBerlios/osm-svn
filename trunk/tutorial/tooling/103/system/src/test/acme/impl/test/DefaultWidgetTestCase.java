cd10e7f4bcf6f9a8460d3d2b6b4d934682e664c3
mcconnell <mcconnell@aaecaf6e-66fb-0310-8020-e30436e8796d>
2005-07-21 22:00:38 +0000

    Getting training content into place.

---
+++ trunk/tutorial/tooling/103/system/src/test/acme/impl/test/DefaultWidgetTestCase.java
@@ -0,0 +1,60 @@
+
+package acme.impl.test;
+
+import junit.framework.TestCase;
+
+import acme.Widget;
+import acme.impl.DefaultWidget;
+
+/**
+ * Test the DefaultWidget
+ *
+ * @author <a href="http://www.osm.net">Open Service Management</a>
+ */
+public class DefaultWidgetTestCase extends TestCase
+{
+   /**
+    * Test the default message produced when we supply a null value.
+    */
+    public void testDefaultMessage() throws Exception
+    {
+        DefaultWidget widget = new DefaultWidget();
+        String result = widget.buildMessage( null );
+        String expected = "I'm a plain old " + COLOR + " widget named " + NAME + ".";
+        assertEquals( "default", expected, result );
+        widget.process( null );
+    }
+
+   /**
+    * Test a non-null value.
+    */
+    public void testExplicitMessage() throws Exception
+    {
+        DefaultWidget widget = new DefaultWidget();
+        String color = "blue";
+        String result = widget.buildMessage( color );
+        String expected = "I'm a " + color + " widget named " + NAME;
+        assertEquals( "explicit", expected, result );
+        widget.process( color );
+    }
+
+   /**
+    * Test the the DefaultWidget is in fact an instance of Widget.
+    */
+    public void testImplementsWidget() throws Exception
+    {
+        DefaultWidget impl = new DefaultWidget();
+        try
+        {
+            Widget widget = (Widget) impl;
+        }
+        catch( ClassCastException e )
+        {
+            fail( "DefaultWidget does not implement the Widget service interface." );
+        }
+    }
+
+    private static final String NAME = "@NAME@";
+    private static final String COLOR = "@COLOR@";
+
+}
