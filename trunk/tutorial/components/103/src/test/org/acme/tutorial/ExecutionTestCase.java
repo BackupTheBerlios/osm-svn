469107f3592aef08ed5bbc3fbf1cbe65244bfb3a
mcconnell <mcconnell@aaecaf6e-66fb-0310-8020-e30436e8796d>
2006-01-17 02:34:22 +0000

    tests, tutorials and docs

---
+++ trunk/tutorial/components/103/src/test/org/acme/tutorial/ExecutionTestCase.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2005-2006 Stephen J. McConnell.
+ *
+ * Licensed  under the  Apache License,  Version 2.0  (the "License");
+ * you may not use  this file  except in  compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.acme.tutorial;
+
+import java.io.File;
+import java.net.URI;
+import java.awt.Color;
+
+import junit.framework.TestCase;
+
+import net.dpml.part.local.Controller;
+import net.dpml.part.remote.Component;
+import net.dpml.part.remote.Provider;
+
+/**
+ * Test a widget component.
+ *
+ * @author <a href="@PUBLISHER-URL@">@PUBLISHER-NAME@</a>
+ * @version @PROJECT-VERSION@
+ */
+public class ExecutionTestCase extends TestCase
+{
+    private static final String PATH = "example.part";
+    private static final File DIRECTORY = new File( System.getProperty( "project.test.dir" ) );
+    private static final Controller CONTROLLER = Controller.STANDARD;
+    
+   /**
+    * Test the cexecution of the widget.
+    * @exception Exception if an error occurs
+    */
+    public void testWidgetExecution() throws Exception
+    {
+        URI uri = new File( DIRECTORY, PATH ).toURI();
+        Component component = CONTROLLER.createComponent( uri );
+        Provider provider = component.getProvider();
+        ExampleComponent instance = (ExampleComponent) provider.getValue( false );
+        Color color = instance.getColor();
+        assertEquals( "color", new Color( 100, 200, 0 ), color );
+    }
+}
