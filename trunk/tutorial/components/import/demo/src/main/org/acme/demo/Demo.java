9ef1477f3cfc90f2f2324f3b017417ab1a0221cb
mcconnell <mcconnell@aaecaf6e-66fb-0310-8020-e30436e8796d>
2006-07-03 14:46:49 +0000

    add demo dealing with import of an external part

---
+++ trunk/tutorial/components/import/demo/src/main/org/acme/demo/Demo.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2006 Stephen J. McConnell.
+ *
+ * Licensed  under the  Apache License,  Version 2.0  (the "License");
+ * you may not use  this file  except in  compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.acme.demo;
+
+import java.util.Date;
+
+import org.acme.Clock;
+
+import net.dpml.logging.Logger;
+
+/**
+ * A minimal component.
+ * 
+ * @author <a href="@PUBLISHER-URL@">@PUBLISHER-NAME@</a>
+ * @version @PROJECT-VERSION@
+ */
+public class Demo
+{
+    //------------------------------------------------------------------
+    // criteria
+    //------------------------------------------------------------------
+    
+    public interface Parts
+    {
+        Clock getClock();
+    }
+    
+    //------------------------------------------------------------------
+    // constructor
+    //------------------------------------------------------------------
+    
+    public Demo( final Logger logger, final Parts parts )
+    {
+        Clock clock = parts.getClock();
+        if( logger.isInfoEnabled() )
+        {
+            logger.info( "located clock: " + clock.getClass().getName() );
+            logger.info( "current time: " + clock.getDate() );
+        }
+    }
+}
