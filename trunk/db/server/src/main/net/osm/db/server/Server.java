1eaba4307986347a88779689539362cfa8cb867a
davidoff <davidoff@aaecaf6e-66fb-0310-8020-e30436e8796d>
2009-10-18 22:28:02 +0000

    Add db server.

---
+++ trunk/db/server/src/main/net/osm/db/server/Server.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright 2007 Stephen J. McConnell
+ *
+ * Licensed  under the  Apache License,  Version 2.0  (the "License");
+ * you may not use  this file  except in  compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.osm.db.server;
+
+import java.io.PrintWriter;
+import java.io.OutputStream;
+import java.io.StringWriter;
+import java.io.File;
+import java.net.InetAddress;
+
+import net.dpml.util.Logger;
+
+import org.apache.derby.drda.NetworkServerControl;
+
+/**
+* Derby shared server implementation.
+*/
+public class Server extends NetworkServerControl
+{
+   /**
+    * Deployment context.
+    */
+    public interface Context
+    {
+       /**
+        * Get the hostname that this server will listen on.  Defaults
+        * to localhost.
+        */
+        String getHost();
+        
+       /**
+        * Get the port number to listner to.  Defaults
+        * to 1527.
+        */
+        int getPort();
+        
+    }
+    
+    private final Logger m_logger;
+    private final Context m_context;
+    
+   /**
+    * Creation of a new shared Derby server.
+    * @param logger the logging channel
+    * @param context the deployment context
+    * @exception if an error occurs
+    */
+    public Server( Logger logger, Context context ) throws Exception
+    {
+        super( InetAddress.getByName( context.getHost() ), context.getPort() );
+        
+        m_context = context;
+        m_logger = logger;
+    }
+    
+   /**
+    * Start the server. This method is invoked automatically by Metro as 
+    * a part of the component initialization lifecycle.
+    * @exception if an error occurs
+    */
+    public void start() throws Exception
+    {
+        m_logger.info( "commencing server startup" );
+        OutputAdapter adapter = new OutputAdapter();
+        PrintWriter writer = new PrintWriter( adapter );
+        super.start( writer );
+        m_logger.info( "server startup complete" );
+    }
+    
+   /**
+    * Stop the server. This method is invoked automatically by Metro as 
+    * a part of the component termination lifecycle.
+    * @exception if an error occurs
+    */
+    public void stop() throws Exception
+    {
+        m_logger.info( "commencing server shutdown" );
+        super.shutdown();
+        m_logger.info( "server shutdown complete" );
+    }
+    
+   /**
+    * Internal adapter to handle redirection of Derby output srream 
+    * messages to the logging channel assigned to the component.
+    */
+    private class OutputAdapter extends OutputStream
+    {
+        private StringWriter m_writer = new StringWriter();
+        
+       /**
+        * Flush the characters as a string to the logging channel
+        * as a debug level message.
+        */
+        public void flush()
+        {
+            synchronized( m_writer )
+            {
+                String message = m_writer.toString();
+                if( message.endsWith( "\n" ) )
+                {
+                    message = message.substring( 0, message.length() - 1 );
+                }
+                if( message.startsWith( "Apache Derby " ) )
+                {
+                    int n = message.indexOf( ") " );
+                    if( n > 0 )
+                    {
+                        message = message.substring( n + 2 );
+                    }
+                }
+                m_writer.getBuffer().setLength( 0 );
+                m_logger.debug( message );
+            }
+        }
+        
+       /**
+        * Write a character to the buffer.
+        * @param b the character value
+        */
+        public void write( int b )
+        {
+            synchronized( m_writer )
+            {
+                m_writer.write( b );
+            }
+        }
+    }
+}
+
---
+++ trunk/db/server/src/test/net/osm/db/server/ExpressTestCase.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2006 Stephen J. McConnell.
+ *
+ * Licensed  under the  Apache License,  Version 2.0  (the "License");
+ * you may not use  this file  except in  compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.osm.db.server;
+
+import dpml.util.DefaultLogger;
+
+import java.io.File;
+import java.net.URI;
+
+import junit.framework.TestCase;
+
+import net.dpml.lang.Strategy;
+import net.dpml.runtime.Component;
+import net.dpml.runtime.Provider;
+import net.dpml.util.Logger;
+
+/**
+ * Express test case.
+ */
+public class ExpressTestCase extends TestCase
+{
+    private Logger m_logger = new DefaultLogger( "test" );
+    private Component m_component;
+    private Server m_server;
+
+    public void setUp() throws Exception
+    {
+        m_logger.info( "commencing setup" );
+        URI uri = getPartURI();
+        Strategy strategy = Strategy.load( uri );
+        m_component = strategy.getInstance( Component.class );
+        Provider provider = m_component.getProvider();
+        m_server = provider.getInstance( Server.class );
+        m_logger.info( "setup complete" );
+    }
+    
+    public void tearDown() throws Exception
+    {
+        m_logger.info( "commencing tear-down" );
+        try
+        {
+            m_component.terminate();
+            m_logger.info( "tear-down complete" );
+         }
+        catch( Throwable e )
+        {
+            m_logger.error( "termination error", e );
+        }
+    }
+    
+    public void testServerPing() throws Exception
+    {   
+        m_logger.info( "executing test" );
+        
+        int n = 0;
+        boolean test = true;
+        while ( test == true ) 
+        {
+            n++;
+            try
+            {
+                m_server.ping();
+                test = false;
+                m_logger.info( "ping resolved after " + n + " cycles" );
+            }
+            catch ( Throwable e )
+            {
+                if( n > 1000 )
+                {
+                    test = false;
+                    m_logger.info( "ping FAILED" );
+                    throw new Exception( "Failed to invoke a successful ping." );
+                }
+            }
+        }
+    }
+    
+    private URI getPartURI() throws Exception
+    {
+        String path = System.getProperty( "basedir" );
+        File file = new File( path );
+        File target = new File( file, "target/test/test.part" );
+        return target.toURI();
+    }
+}
+
